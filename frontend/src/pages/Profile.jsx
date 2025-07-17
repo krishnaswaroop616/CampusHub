87087f3fe134008de12eb8998147ba377131b90b
T Krishna Swaroop <krishnaswaroop431@gmail.com>
2025-07-18 00:42:54 +0530

    Update Profile.jsx

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -216,7 +216,7 @@ const Profile = () => {
 
     }
 
-    if (!user) return <div className="text-center mt-5 fs-2">Loading profile...</div>;
+    if (!user) return <div className="text-center mt-5 fs-2 text-white">Loading profile...</div>;
 
     return (
         <div className="container mt-4">
